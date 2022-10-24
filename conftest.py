313f038c28bcc852662ae8019527cbac39279766
Everize <Everize1@gmail.com>
2022-10-24 22:00:51 +0200

    Add BasePage function to catch NoSucNoSuchElementException

--- conftest.py
+++ conftest.py
@@ -41,7 +41,7 @@ def browser(request):
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
 
-    browser.implicitly_wait(10)
     yield browser
+
     print("\nQuit browser..")
     browser.quit()
