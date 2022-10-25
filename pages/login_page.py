d7769d0ea34254e92171e7061b9eebaaf4d7c415
Everize <Everize1@gmail.com>
2022-10-25 20:59:28 +0200

    LoginPage methods to check url, login and register form. MainPage cleanup

--- pages/login_page.py
+++ pages/login_page.py
@@ -11,12 +11,12 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert LoginPageLocators.LOGIN_LINK in self.browser.current_url
+        assert "login" in self.browser.current_url
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert self.find_element(LoginPageLocators.LOGIN_FORM)
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM)
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert self.find_element(LoginPageLocators.REGISTER_FORM)
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM)
